08d5a141c2aa0ce01739bc56e622e76d795f4c3d
Claire Chang <cochia0318@hotmail.com>
2018-10-13 11:48:21 +0800

    10/29 PIXI場景設定

---
+++ src/Main.ts
@@ -0,0 +1,36 @@
+import Application = PIXI.Application;
+
+export let application:Application;
+/**
+ * 主要的 client application.
+ *
+ */
+export class Main {
+
+    public initGame() {
+       //設定場景
+       let gameCanvasContext = (< HTMLCanvasElement >jQuery("#gameCanvas")[0]);
+       application = new PIXI.Application(960, 540, {backgroundColor : 0x000000, view: gameCanvasContext});
+       
+       //貼一張圖片
+       var bunny = PIXI.Sprite.fromImage('assets/bunny.png');
+       bunny.x = application.screen.width / 2;
+       bunny.y = application.screen.height / 2;
+       application.stage.addChild(bunny);
+       
+       //設定遊戲大小隨視窗大小改變
+       this.onResize(); 
+       window.onresize = this.onResize; 
+    }
+
+    public onResize() { 
+        var w = window.innerWidth; 
+        var h = window.innerHeight; 
+        var scale = Math.min(w/860,h/540); 
+        application.view.style.left = (w-scale*860)/2 + "px"; 
+        application.view.style.top = (h-scale*540)/2 + "px"; 
+        application.view.style.width = scale*860 + "px"; 
+        application.view.style.height = scale*540 + "px"; 
+         
+    } 
+}
